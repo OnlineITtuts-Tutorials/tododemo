5ccb2117c98dcb47154b623c338ac1a695704191
OnlineITtuts-Tutorials <asifwebdeveloper@hotmail.com>
2025-10-21 12:28:21 +0500

    update url

--- client/src/api/tasksApi.js
+++ client/src/api/tasksApi.js
@@ -1,5 +1,5 @@
 // const API_BASE_URL = "http://localhost:3000";
-const API_BASE_URL = "";
+const API_BASE_URL = "https://api-eta-topaz-15.vercel.app";
 
 // Fetch all tasks
 export const fetchTasksApi = async () => {
